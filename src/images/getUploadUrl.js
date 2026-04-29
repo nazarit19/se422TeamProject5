19c79fe6cb01c79412b6035e286553d3a2d2d447
nazarit19 <138739227+nazarit19@users.noreply.github.com>
2026-04-28 20:58:34 -0500

    lambda for images

--- src/images/getUploadUrl.js
+++ src/images/getUploadUrl.js
@@ -1,11 +1,27 @@
+const { S3Client, PutObjectCommand } = require('@aws-sdk/client-s3')
+const { getSignedUrl } = require('@aws-sdk/s3-request-presigner')
+const { randomUUID } = require('crypto')
+
+const s3 = new S3Client({})
+
 exports.handler = async (event) => {
+  const userId = event.requestContext?.authorizer?.claims?.sub
+  const { fileType } = JSON.parse(event.body) // e.g. "image/jpeg"
+
+  const key = `listings/${userId}/${randomUUID()}`
+
+  const url = await getSignedUrl(s3, new PutObjectCommand({
+    Bucket: process.env.IMAGES_BUCKET,
+    Key: key,
+    ContentType: fileType
+  }), { expiresIn: 60 }) // URL expires in 60 seconds
+
   return {
     statusCode: 200,
-    headers: {
-      'Access-Control-Allow-Origin': '*',
-      'Access-Control-Allow-Headers': 'Content-Type,Authorization',
-      'Access-Control-Allow-Methods': 'GET,POST,PUT,DELETE,OPTIONS'
-    },
-    body: JSON.stringify([])
+    headers: CORS_HEADERS,
+    body: JSON.stringify({
+      uploadUrl: url,
+      imageUrl: `https://${process.env.IMAGES_BUCKET}.s3.amazonaws.com/${key}`
+    })
   }
 }
